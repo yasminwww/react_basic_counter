817de0723655738158c4b6fa60c0e7b1b7c6b5ed
Yasmin <ym222bs@student.lnu.se>
2019-03-14 22:55:36 +0100

    added component copies

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 // can add export default instead of exporting each class
 class Counter extends Component {
+   
     state = {
         count: 0,
         tags: ['tag1', 'tag2', 'tag3']
@@ -12,30 +13,38 @@ class Counter extends Component {
         fontWeight: 'bold'
     }
 
+    // renderTags() {
+    //     if (this.state.tags.length === 0) return <p>There are no tags</p>
+
+    //     return <ul>{this.state.tags.map(tag => <li key={tag} >{tag}</li>)}</ul>
+    // }
+
+    handleIncrement = () => {
+        this.setState({count: this.state.count + 1})
+    }
+
+
+
     render() {
         return (
             <div>
-                <span style={this.styles} className={this.getBadgeClasses()}>
-                    {this.formatCount()}
-                </span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag} >{tag}</li>)}
-                </ul>
+                <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                {/* {this.renderTags()} */}
             </div>
         )
     }
 
-    getBadgeClasses() {
-        let classes = 'badge m-2 badge-'
-        classes += this.state.count === 0 ? 'warning' : 'primary'
-        return classes
-    }
+        getBadgeClasses() {
+            let classes = 'badge m-2 badge-'
+            classes += this.state.count === 0 ? 'warning' : 'primary'
+            return classes
+        }
 
-    formatCount() {
-        const { count } = this.state
-        return count === 0 ? 'Zero' : count
-    }
+        formatCount() {
+            const { count } = this.state
+            return count === 0 ? 'Zero' : count
+        }
 }
 
 export default Counter
