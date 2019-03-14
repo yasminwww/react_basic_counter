0296b8f4c468b0676d4dfb827cbf021844dd4488
Yasmin <ym222bs@student.lnu.se>
2019-03-15 00:28:36 +0100

    moved state, 1:55

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,28 +2,18 @@ import React, { Component } from 'react'
 
 // can add export default instead of exporting each class
 class Counter extends Component {
-   
-    state = {
-        value: this.props.counter.value
-    }
 
     styles = {
         fontSize: 20,
         fontWeight: 'bold'
     }
 
-    handleIncrement = () => {
-        this.setState({value: this.state.value + 1})
-    }
-
-
-
     render() {
         
         return (
             <div>
                 <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
                 <button onClick={() => this.props.onDelete(this.props.counter.id) } className='btn btn-danger btn-sm m-2'>Delete</button>
             </div>
         )
@@ -31,14 +21,15 @@ class Counter extends Component {
 
         getBadgeClasses() {
             let classes = 'badge m-2 badge-'
-            classes += this.state.value === 0 ? 'warning' : 'primary'
+            classes += this.props.counter.value === 0 ? 'warning' : 'primary'
             return classes
         }
 
         formatCount() {
-            const { value: count } = this.state
-            return count === 0 ? 'Zero' : count
+            const { value } = this.props.counter
+            return value === 0 ? 'Zero' : value
         }
 }
+
 
 export default Counter
